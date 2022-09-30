20200930 sync -- fixed underflow error (of control signal) in free-running PLL that acts as a reference

dicts_lib_3rdGen.py
@@ -31,21 +31,21 @@ gc.enable()
 def getDicts(Fsim=55):
 
 	dict_net = {
-		'Nx': 3,																# oscillators in x-direction
+		'Nx': 4,																# oscillators in x-direction
 		'Ny': 1,																# oscillators in y-direction
 		'mx': 0,																# twist/chequerboard in x-direction (depends on closed or open boundary conditions)
 		'my': -999,																# twist/chequerboard in y-direction
-		'topology': 'chain',													# 1d) ring, chain, 2d) square-open, square-periodic, hexagonal...
+		'topology': 'entrainOne-chain',													# 1d) ring, chain, 2d) square-open, square-periodic, hexagonal...
 																				# 3) global, entrainOne-[ring,chain,square-open], entrainAll-[ring,chain,square-open], entrainPLLsHierarch, compareEntrVsMutual
 		'normalize_mutual_coupling_by_inputs': True,  							# divide the sum over all inputs to a PLL by the number of inputs YES/NO -> True/False
 		'Tsim': 20000,															# simulation time in multiples of the period
 		'computeFreqAndStab': False,											# compute linear stability and global frequency if possible: True or False
 		'phi_array_mult_tau': 1,												# how many multiples of the delay is stored of the phi time series
-		'phiPerturb': [0 for i in range(3)],									# delta-perturbation on initial state -- PROVIDE EITHER ONE OF THEM! if [] set to zero
+		'phiPerturb': [0 for i in range(4)],									# delta-perturbation on initial state -- PROVIDE EITHER ONE OF THEM! if [] set to zero
 		'phiPerturbRot': [],													# delta-perturbation on initial state -- in rotated space
-		'phiInitConfig': [0 for i in range(3)],									# phase-configuration of sync state,  []: automatic, else provide list
+		'phiInitConfig': [0 for i in range(4)],									# phase-configuration of sync state,  []: automatic, else provide list
 		'freq_beacons': 0.25,													# frequency of external sender beacons, either a float or a list
-		'special_case': 'False',#'timeDepChangeOfIntrFreq', #'timeDepTransmissionDelay', #'False'					# 'False', or 'test_case', 'timeDepInjectLockCoupStr', 'timeDepTransmissionDelay', 'timeDepChangeOfCoupStr', 'distanceDepTransmissionDelay', 'timeDepChangeOfIntrFreq'
+		'special_case': 'timeDepChangeOfIntrFreq', #'timeDepTransmissionDelay', #'False'					# 'False', or 'test_case', 'timeDepInjectLockCoupStr', 'timeDepTransmissionDelay', 'timeDepChangeOfCoupStr', 'distanceDepTransmissionDelay', 'timeDepChangeOfIntrFreq'
 		'typeOfTimeDependency': 'linear', #'triangle',										# adiabatic-{'exponential', 'linear', 'quadratic', 'triangle', 'cosine'} vs freerun-{'exponential', 'linear', 'quadratic', 'triangle', 'cosine'}
 		'min_max_rate_timeDepPara': [0.985, 1.015, 0.00001/100] #[1.2, 3.4, 0.0005/100] 	#[760, 1800, 0.5/100] # provide a list with min, max and rate of the time-dependent parameter
 	}
@@ -56,7 +56,7 @@ def getDicts(Fsim=55):
 		'coupK': 0.01152, 														# [random.uniform(0.3, 0.4) for i in range(dict_net['Nx']*dict_net['Ny'])],# coupling strength (like phase model: K = Kvco/2 * G_all, NOTE: the /2 is for coupling functions that have peak2peal amplitude 2) in Hz float or [random.uniform(minK, maxK) for i in range(dict_net['Nx']*dict_net['Ny'])]
 		'gPDin': 1,																# gains of the different inputs to PD k from input l -- G_kl, see PD, set to 1 and all G_kl=1 (so far only implemented for some cases, check!): np.random.uniform(0.95,1.05,size=[dict_net['Nx']*dict_net['Ny'],dict_net['Nx']*dict_net['Ny']])
 		'gPDin_symmetric': True,												# set to True if G_kl == G_lk, False otherwise
-		'cutFc': 4.045E-5, #0.0207,												# LF cut-off frequency in Hz, here N=9 with mean 0.015: [0.05,0.015,0.00145,0.001,0.0001,0.001,0.00145,0.015,0.05], [random.uniform(0.001, 0.0015) for i in range(dict_net['Nx']*dict_net['Ny'])]
+		'cutFc': 0.0207, #4.045E-5, #0.0207,												# LF cut-off frequency in Hz, here N=9 with mean 0.015: [0.05,0.015,0.00145,0.001,0.0001,0.001,0.00145,0.015,0.05], [random.uniform(0.001, 0.0015) for i in range(dict_net['Nx']*dict_net['Ny'])]
 		'orderLF': 2,															# order of LF filter, either 1 or 2 at the moment
 		'div': 1,																# divisor of divider (int)
 		'friction_coefficient': 1,												# friction coefficient of 2nd order Kuramoto models

sim_pll/pll_lib.py
@@ -127,22 +127,28 @@ class LowPassFilter:
 			self.cutoff_freq_rad = 2.0 * np.pi * self.cutoff_freq_Hz
 			self.beta = self.dt * self.cutoff_freq_rad
 
-			if self.order_loop_filter == 1:
-				print('I am the loop filter of PLL%i: first order, a=%i. Friction coefficient set to %0.2f.' % (self.pll_id, self.order_loop_filter, self.friction_coefficient))
-				#sys.exit('I WENT HERE! self.cutoff_freq_Hz=%s'%self.cutoff_freq_Hz)
-				self.evolve = lambda xPD: (1.0 - self.beta * self.friction_coefficient) * self.control_signal + self.beta * xPD
-				self.b = 1.0 / (2.0 * np.pi * self.cutoff_freq_Hz)
-			elif self.order_loop_filter >= 2:
-				if integrate_2nd_order_diff_eq:
-					print('I am the loop filter of PLL%i: order, a=%i (sequential RC LFs without buffer, using solve_ivp for 2 coupled first order ODEs). Friction coefficient set to %0.2f.' % (self.pll_id, self.order_loop_filter, self.friction_coefficient))
-					self.evolve = lambda xPD: self.solve_2nd_order_ordinary_diff_eq(xPD)
-					a = self.order_loop_filter
-					self.b = 1.0 / (2.0 * np.pi * self.cutoff_freq_Hz * a)
-					self.t = np.array([0, self.dt])
-				else:
-					print('I am the loop filter of PLL%i: order, a=%i (sequential RC LFs with buffers). Friction coefficient set to %0.2f.' % (self.pll_id, self.order_loop_filter, self.friction_coefficient))
-					self.evolve = lambda xPD: self.nth_order_sequential_lf(xPD)
+			# for a simulation that involves the entrainment by a reference oscillator, the oscillator with k=0 is chosen to be the reference: then we omit the filter as this can lead to underflow errors as
+			# the control signal becomes smaller and smaller until it cannot be represented by a np.float64 anymore which leads to an error
+			if 'entrain' in dict_net['topology'] and self.pll_id == 0:
+				print('I am the reference, hence simulating without loop filter to prevent underflow errors with the control signal!')
+				self.evolve = lambda xPD: xPD
+			else:
+				if self.order_loop_filter == 1:
+					print('I am the loop filter of PLL%i: first order, a=%i. Friction coefficient set to %0.2f.' % (self.pll_id, self.order_loop_filter, self.friction_coefficient))
+					#sys.exit('I WENT HERE! self.cutoff_freq_Hz=%s'%self.cutoff_freq_Hz)
+					self.evolve = lambda xPD: (1.0 - self.beta * self.friction_coefficient) * self.control_signal + self.beta * xPD
 					self.b = 1.0 / (2.0 * np.pi * self.cutoff_freq_Hz)
+				elif self.order_loop_filter >= 2:
+					if integrate_2nd_order_diff_eq:
+						print('I am the loop filter of PLL%i: order, a=%i (sequential RC LFs without buffer, using solve_ivp for 2 coupled first order ODEs). Friction coefficient set to %0.2f.' % (self.pll_id, self.order_loop_filter, self.friction_coefficient))
+						self.evolve = lambda xPD: self.solve_2nd_order_ordinary_diff_eq(xPD)
+						a = self.order_loop_filter
+						self.b = 1.0 / (2.0 * np.pi * self.cutoff_freq_Hz * a)
+						self.t = np.array([0, self.dt])
+					else:
+						print('I am the loop filter of PLL%i: order, a=%i (sequential RC LFs with buffers). Friction coefficient set to %0.2f.' % (self.pll_id, self.order_loop_filter, self.friction_coefficient))
+						self.evolve = lambda xPD: self.nth_order_sequential_lf(xPD)
+						self.b = 1.0 / (2.0 * np.pi * self.cutoff_freq_Hz)
 		elif self.cutoff_freq_Hz is None or self.cutoff_freq_Hz == 0:
 			print('No cut-off frequency defined (cutFc=%s), hence simulating without loop filter!' % self.cutoff_freq_Hz)
 			self.evolve = lambda xPD: xPD
@@ -151,7 +157,7 @@ class LowPassFilter:
 			sys.exit()
 
 
-	def nth_order_sequential_lf(self, phase_detector_output: float) -> np.float:
+	def nth_order_sequential_lf(self, phase_detector_output: np.float64) -> np.float:
 		""" Evolves the output of an n-th order loop filter by one time increment.
 
 		Args:
@@ -162,8 +168,11 @@ class LowPassFilter:
 		"""
 		for i in range(self.order_loop_filter):									# apply first order loop filter (identical RC) sequentially
 			# print('i=',i)
+			# print('self.control_signal: ', self.control_signal, ' self.beta: ', self.beta)
 			phase_detector_output = (1.0 - self.beta * self.friction_coefficient) * self.control_signal + self.beta * phase_detector_output
+			# print('osci%i phase_detector_output %i/1' % (self.pll_id, i), phase_detector_output)
 			self.control_signal = phase_detector_output							# needs to be updated in between!
+			# print('osci%i phase_detector_output %i/2' % (self.pll_id, i), phase_detector_output)
 
 		# prior_ctrl_sig = self.control_signal
 		# for i in range(self.order_loop_filter):									# apply first order loop filter (identical RC) sequentially
9a179153379148bdaff68d237bbadd41d40fffac
cuichi23 <lucas_we@hotmail.com>
2022-10-05 12:40:00 +0200

    20221005

--- dicts_lib_entrainMutC.py
+++ dicts_lib_entrainMutC.py
@@ -38,7 +38,7 @@ def getDicts(Fsim=55):
 		'topology': 'entrainOne-chain',											# 1d) ring, chain, 2d) square-open, square-periodic, hexagonal...
 																				# 3) global, entrainOne-[ring,chain,square-open], entrainAll-[ring,chain,square-open], entrainPLLsHierarch, compareEntrVsMutual
 		'normalize_mutual_coupling_by_inputs': True,  							# divide the sum over all inputs to a PLL by the number of inputs YES/NO -> True/False
-		'Tsim': 50,															# simulation time in multiples of the period
+		'Tsim': 4400,															# simulation time in multiples of the period
 		'computeFreqAndStab':  False,											# compute linear stability and global frequency if possible: True or False
 		'phi_array_mult_tau': 1,												# how many multiples of the delay is stored of the phi time series
 		'phiPerturb': [random.uniform(0, 1E-9) for i in range(3)],				# delta-perturbation on initial state -- PROVIDE EITHER ONE OF THEM! if [] set to zero
@@ -70,7 +70,7 @@ def getDicts(Fsim=55):
 		# choose from coupfct.<ID>: sine, cosine, neg_sine, neg_cosine, triangular, deriv_triangular, square_wave, pfd, inverse_cosine, inverse_sine
 		'coup_fct_sig': coupfct.triangular,	#coupfct.sine,						# coupling function h(x) for PLLs with ideally filtered PD signals:
 		'derivative_coup_fct': coupfct.deriv_triangular,						# derivative h'(x) of coupling function h(x)
-		'branch_of_inverse_coupling_fct_if_applies': 'positive',				# choose the branch of the coupling function, e.g., for triangular: 'negative' or 'positive'
+		'branch_of_inverse_coupling_fct_if_applies': 'negative',				# choose the branch of the coupling function, e.g., for triangular: 'negative' or 'positive'
 		'includeCompHF': False,													# boolean True/False whether to simulate with HF components
 		'vco_out_sig': coupfct.square_wave_symm_zero,							# for HF case, e.g.: coupfct.sine, coupfct.square_wave, coupfct.square_wave_symm_zero
 		'typeVCOsig': 'analogHF',												# 'analogHF' or 'digitalHF' - determines whether XOR PD or multiplier is used
@@ -98,14 +98,14 @@ def getDicts(Fsim=55):
 
 	dict_algo = {
 		'parameter_space_sweeps': 'two_parameter_sweep',						# pick method for setting realizations 'single', 'classicBruteForceMethodRotatedSpace', 'listOfInitialPhaseConfigurations', 'two_parameter_sweep'
-		'paramDiscretization': [3, 3],										# parameter discretization for brute force initial phases and parameter space scans, e.g., for 'listOfInitialPhaseConfigurations' it specifies the number of initial phase differences and the discretization points for 'min_max_range_parameter_0'
+		'paramDiscretization': [13, 13],										# parameter discretization for brute force initial phases and parameter space scans, e.g., for 'listOfInitialPhaseConfigurations' it specifies the number of initial phase differences and the discretization points for 'min_max_range_parameter_0'
 		'param_id_0': 'intrF',													# parameter to be changed between different realizations, according to the min_max_range_parameter: 'None' or string of any other parameter
 		'min_max_range_parameter_0': [0.55, 1.45],								# specifies within which min and max value to linspace the, e.g., initial frequency difference (w.r.t. HF Frequency, not divided) in the case of 'listOfInitialPhaseConfigurations', or any other parameter as in, e.g., 'two_parameter_sweep'
 		'param_id_1': 'transmission_delay',  									# parameter to be changed between different realizations, according to the min_max_range_parameter: 'None' or string of any other parameter
 		'min_max_range_parameter_1': [0.01, 2.6],  								# specifies within which min and max value to linspace the, e.g., initial frequency difference (w.r.t. HF Frequency, not divided) in the case of 'listOfInitialPhaseConfigurations', or any other parameter as in, e.g., 'two_parameter_sweep'
 		'store_ctrl_and_clock': True,											# whether the control signals and clock signal is being computed (time and memory usage)
 		'store_phases_tau_array': True,											# whether the phases are saved when simulation on tau-array
-		'number_of_processes_in_multisim': 6									# choose there an integer number that specifies the number of parallel threads to be simulated
+		'number_of_processes_in_multisim': 16									# choose there an integer number that specifies the number of parallel threads to be simulated
 	}
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 

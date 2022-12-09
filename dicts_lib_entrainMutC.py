20221210 bugfix phase relation calculation

dicts_lib_entrainMutC.py
@@ -38,7 +38,7 @@ def getDicts(Fsim: int = 55):
 		'topology': 'entrainOne-chain',											# 1d) ring, chain, 2d) square-open, square-periodic, hexagonal...
 																				# 3) global, entrainOne-[ring,chain,square-open], entrainAll-[ring,chain,square-open], entrainPLLsHierarch, compareEntrVsMutual
 		'normalize_mutual_coupling_by_inputs': True,  							# divide the sum over all inputs to a PLL by the number of inputs YES/NO -> True/False
-		'Tsim': 4400,															# simulation time in multiples of the period
+		'Tsim': 75,															# simulation time in multiples of the period
 		'computeFreqAndStab':  False,											# compute linear stability and global frequency if possible: True or False
 		'phi_array_mult_tau': 1,												# how many multiples of the delay is stored of the phi time series
 		'phiPerturb': [random.uniform(0, 1E-9) for i in range(3)],				# delta-perturbation on initial state -- PROVIDE EITHER ONE OF THEM! if [] set to zero
@@ -51,26 +51,26 @@ def getDicts(Fsim: int = 55):
 	}
 
 	dict_pll = {
-		'intrF': [1, 1.004, 0.996],												# intrinsic frequency in Hz: Note that always he first entry is that of the reference!
-		'syncF': 1,																# frequency of synchronized state in Hz
-		'coupK': [0., 0.408, 0.4225],											# [random.uniform(0.3, 0.4) for i in range(dict_net['Nx']*dict_net['Ny'])],# coupling strength (like phase model: K = Kvco/2 * G_all, NOTE: the /2 is for coupling functions that have peak2peal amplitude 2) in Hz float or [random.uniform(minK, maxK) for i in range(dict_net['Nx']*dict_net['Ny'])]
+		'intrF': [0.865, 1.004, 0.996],											# intrinsic frequency in Hz: Note that always he first entry is that of the reference!
+		'syncF': 1,															    # frequency of synchronized state in Hz
+		'coupK': [0., 0.408/2, 0.4225/2],											# [random.uniform(0.3, 0.4) for i in range(dict_net['Nx']*dict_net['Ny'])],# coupling strength (like phase model: K = Kvco/2 * G_all, NOTE: the /2 is for coupling functions that have peak2peal amplitude 2) in Hz float or [random.uniform(minK, maxK) for i in range(dict_net['Nx']*dict_net['Ny'])]
 		'gPDin': 1.0,	#8.5													# gains of the different inputs to PD k from input l -- G_kl, see PD, set to 1 and all G_kl=1 (so far only implemented for some cases, check!): np.random.uniform(0.95,1.05,size=[dict_net['Nx']*dict_net['Ny'],dict_net['Nx']*dict_net['Ny']])
 		'gPDin_symmetric': False,												# set to True if G_kl == G_lk, False otherwise
-		'cutFc': [0., 0.0156, 0.0156],											# LF cut-off frequency in Hz, None for no LF, or e.g., N=9 with mean 0.015: [0.05,0.015,0.00145,0.001,0.0001,0.001,0.00145,0.015,0.05], [0.0148, 0.0148, 0.0957, 0.0957, 0.0148, 0.0148, 0.0957, 0.0957, 0.0148, 0.0148, 0.0957, 0.0957, 0.0148, 0.0148, 0.0957, 0.0957] # [0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957], #
+		'cutFc': [100, 0.0156*10, 0.0156*10],											# LF cut-off frequency in Hz, None for no LF, or e.g., N=9 with mean 0.015: [0.05,0.015,0.00145,0.001,0.0001,0.001,0.00145,0.015,0.05], [0.0148, 0.0148, 0.0957, 0.0957, 0.0148, 0.0148, 0.0957, 0.0957, 0.0148, 0.0148, 0.0957, 0.0957, 0.0148, 0.0148, 0.0957, 0.0957] # [0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957, 0.0148, 0.0957], #
 		'orderLF': 1,															# order of LF filter, either 1 or 2 at the moment (not compatible with synctools!)
 		'div': 1,																# divisor of divider (int)
 		'friction_coefficient': 1,												# friction coefficient of 2nd order Kuramoto models
 		'fric_coeff_PRE_vs_PRR': 'PRE',											# 'PRR': friction coefficient multiplied to instant. AND intrin. freq, 'PRE': friction coefficient multiplied only to instant. freq
-		'noiseVarVCO': [0, 5E-10, 1E-9],										# variance of VCO GWN
+		'noiseVarVCO': [0, 5E-10, 1E-10],										# variance of VCO GWN
 		'feedback_delay': 0,													# value of feedback delay in seconds
 		'feedback_delay_var': None, 											# variance of feedback delay
-		'transmission_delay': 0.256, #5.0, 										# value of transmission delay in seconds, float (single), list (tau_k) or list of lists (tau_kl): np.random.uniform(min,max,size=[dict_net['Nx']*dict_net['Ny'],dict_net['Nx']*dict_net['Ny']]), OR [np.random.uniform(min,max) for i in range(dict_net['Nx']*dict_net['Ny'])]
+		'transmission_delay': 0.349, #0.256, #5.0, 								# value of transmission delay in seconds, float (single), list (tau_k) or list of lists (tau_kl): np.random.uniform(min,max,size=[dict_net['Nx']*dict_net['Ny'],dict_net['Nx']*dict_net['Ny']]), OR [np.random.uniform(min,max) for i in range(dict_net['Nx']*dict_net['Ny'])]
 		'transmission_delay_var': None, 										# variance of transmission delays
 		'distribution_for_delays': None,										# from what distribution are random delays drawn?
 		# choose from coupfct.<ID>: sine, cosine, neg_sine, neg_cosine, triangular, deriv_triangular, square_wave, pfd, inverse_cosine, inverse_sine
 		'coup_fct_sig': coupfct.triangular,	#coupfct.sine,						# coupling function h(x) for PLLs with ideally filtered PD signals:
 		'derivative_coup_fct': coupfct.deriv_triangular,						# derivative h'(x) of coupling function h(x)
-		'branch_of_inverse_coupling_fct_if_applies': 'negative',				# choose the branch of the coupling function, e.g., for triangular: 'negative' or 'positive'
+		'branch_of_inverse_coupling_fct_if_applies': 'positive',				# choose the branch of the coupling function, e.g., for triangular: 'negative' or 'positive'
 		'includeCompHF': False,													# boolean True/False whether to simulate with HF components
 		'vco_out_sig': coupfct.square_wave_symm_zero,							# for HF case, e.g.: coupfct.sine, coupfct.square_wave, coupfct.square_wave_symm_zero
 		'typeVCOsig': 'analogHF',												# 'analogHF' or 'digitalHF' - determines whether XOR PD or multiplier is used
@@ -98,8 +98,8 @@ def getDicts(Fsim: int = 55):
 
 	dict_algo = {
 		'parameter_space_sweeps': 'two_parameter_sweep',						# pick method for setting realizations 'single', 'classicBruteForceMethodRotatedSpace', 'listOfInitialPhaseConfigurations', 'two_parameter_sweep', 'one_parameter_sweep'
-		'paramDiscretization': [25, 25],										# parameter discretization for brute force initial phases and parameter space scans, e.g., for 'listOfInitialPhaseConfigurations' it specifies the number of initial phase differences and the discretization points for 'min_max_range_parameter_0'
-		'param_id_0': 'intrF',													# parameter to be changed between different realizations, according to the min_max_range_parameter: 'None' or string of any other parameter
+		'paramDiscretization': [16, 16],										# parameter discretization for brute force initial phases and parameter space scans, e.g., for 'listOfInitialPhaseConfigurations' it specifies the number of initial phase differences and the discretization points for 'min_max_range_parameter_0'
+		'param_id_0': 'intrF',  												# parameter to be changed between different realizations, according to the min_max_range_parameter: 'None' or string of any other parameter
 		'min_max_range_parameter_0': [0.55, 1.45],								# specifies within which min and max value to linspace the, e.g., initial frequency difference (w.r.t. HF Frequency, not divided) in the case of 'listOfInitialPhaseConfigurations', or any other parameter as in, e.g., 'two_parameter_sweep'
 		'param_id_1': 'transmission_delay',  									# parameter to be changed between different realizations, according to the min_max_range_parameter: 'None' or string of any other parameter
 		'min_max_range_parameter_1': [0.01, 2.6],  								# specifies within which min and max value to linspace the, e.g., initial frequency difference (w.r.t. HF Frequency, not divided) in the case of 'listOfInitialPhaseConfigurations', or any other parameter as in, e.g., 'two_parameter_sweep'

sim_pll/entrain_mutual_lib.py
@@ -148,7 +148,7 @@ def phase_configuration_ref_to_one_for_chain_topology(dict_net: dict, dict_pll: 
 			#print('Now processing the remaining!')
 			asymptotic_phase_differences_entrained_sync_state[i] = -2*np.pi*dict_pll['intrF'][0]*dict_pll['transmission_delay'] - dict_pll['div']*dict_pll['inverse_coup_fct_sig'](
 				(2*(dict_pll['inverse_fct_vco_response'](dict_pll['intrF'][0]) - frequency_or_voltage[i]))/dict_pll['coupK'][i] - dict_pll['coup_fct_sig'](
-					(-2*np.pi*dict_pll['intrF'][0]*dict_pll['transmission_delay'] - asymptotic_phase_configuration_entrained_sync_state[i+1]) / dict_pll['div']),
+					(-2*np.pi*dict_pll['intrF'][0]*dict_pll['transmission_delay'] + asymptotic_phase_differences_entrained_sync_state[i+1]) / dict_pll['div']),
 						branch=dict_pll['branch_of_inverse_coupling_fct_if_applies'], phase_wrap=phase_wrap)
 
 	# calculate the phase-offsets from the phase-differences for all oscillators, assuming that beta_0 = 0!
@@ -157,21 +157,22 @@ def phase_configuration_ref_to_one_for_chain_topology(dict_net: dict, dict_pll: 
 		# NOTE: asymptotic_phase_differences_entrained_sync_state has been filled from the end towards the beginning... the first entry is 0, representing beta_0=0
 		# so we start \beta_
 
-		asymptotic_phase_configuration_entrained_sync_state[i] = asymptotic_phase_configuration_entrained_sync_state[i-1] - asymptotic_phase_differences_entrained_sync_state[i]
+		asymptotic_phase_configuration_entrained_sync_state[i] = asymptotic_phase_configuration_entrained_sync_state[i-1] + asymptotic_phase_differences_entrained_sync_state[i]
 
-	shift2piWin = 0.0
-	if phase_wrap == 1:  # plot phase-differences in [-pi, pi] interval
-		shift2piWin = np.pi
-	elif phase_wrap == 2:  # plot phase-differences in [-pi/2, 3*pi/2] interval
-		shift2piWin = 0.5 * np.pi
-	elif phase_wrap == 3:  # plot phase-differences in [0, 2*pi] interval
+	if not any(np.isnan(asymptotic_phase_differences_entrained_sync_state)):
 		shift2piWin = 0.0
-
-	asymptotic_phase_configuration_entrained_sync_state = [(asymptotic_phase_configuration_entrained_sync_state[i] + shift2piWin) % (2*np.pi) - shift2piWin for i in range(len(asymptotic_phase_configuration_entrained_sync_state))]
+		if phase_wrap == 1:  # plot phase-differences in [-pi, pi] interval
+			shift2piWin = np.pi
+		elif phase_wrap == 2:  # plot phase-differences in [-pi/2, 3*pi/2] interval
+			shift2piWin = 0.5 * np.pi
+		elif phase_wrap == 3:  # plot phase-differences in [0, 2*pi] interval
+			shift2piWin = 0.0
+		asymptotic_phase_configuration_entrained_sync_state = [((asymptotic_phase_configuration_entrained_sync_state[i] + shift2piWin) % (2.0 * np.pi) - shift2piWin) for i in
+															   range(len(asymptotic_phase_configuration_entrained_sync_state))]
 
 	if not any(np.isnan(asymptotic_phase_differences_entrained_sync_state)):
 		print('\nComputed initial phase-differences for {tau=%0.2f, fR=%0.2f} to be dphi_kl=phi_k-phi_l' % (dict_pll['transmission_delay'],
-																							dict_pll['intrF'][0]), asymptotic_phase_differences_entrained_sync_state)
+																							dict_pll['intrF'][0]), asymptotic_phase_differences_entrained_sync_state[1:])
 		print('\nComputed initial phases for {tau=%0.2f, fR=%0.2f} to be phi_k=' % (dict_pll['transmission_delay'],
 																							dict_pll['intrF'][0]), asymptotic_phase_configuration_entrained_sync_state)
 		# print('dict_pll[*inverse_fct_vco_response*](dict_pll[*intrF*][0])', dict_pll['inverse_fct_vco_response'](dict_pll['intrF'][0]))

sim_pll/evaluation_lib.py
@@ -222,7 +222,10 @@ def calcSpectrum(phase_or_signal: np.ndarray, dict_pll: dict, dict_net: dict, di
 	P0 = 1E-3														# 1000 mW
 	R = 50 															# 50 Ohms --> for P0 in [mW/Hz] and R [ohm]
 
-	Pxx_dBm.append(10*np.log10((Vxx/R)/P0))
+	try:
+		Pxx_dBm.append(10*np.log10((Vxx/R)/P0))
+	except:
+		print('ERROR in PSD computation caught via try/except! check Vxx:', Vxx)
 	f.append(ftemp)
 
 	return f, Pxx_dBm

sim_pll/multisim_lib.py
@@ -105,11 +105,17 @@ def prepare_multiple_simulations(dict_net: dict, dict_pll: dict, dict_algo: dict
 			sys.exit()
 
 	elif dict_algo['parameter_space_sweeps'] == 'single':
-		if dict_algo['param_id_0'] == 'None':
-			dict_algo.update({'min_max_range_parameter_0': [1, 1], 'paramDiscretization': [1, 1]})
+		# if dict_algo['param_id_0'] == 'None':
+		# 	dict_algo.update({'min_max_range_parameter_0': [1, 1], 'paramDiscretization': [1, 1]})
+		# 	print('No parameter to be changed, simulate only one realization!')
+		if dict_algo['param_id_0'] == 'None' and dict_algo['param_id_1'] == 'None':
+			dict_algo.update({'min_max_range_parameter_0': [1, 1], 'min_max_range_parameter_1': [1, 1], 'paramDiscretization': [1, 1]})
 			print('No parameter to be changed, simulate only one realization!')
+		elif dict_algo['param_id_0'] != 'None' or dict_algo['param_id_1'] != 'None':
+			dict_algo.update({'min_max_range_parameter_0': [1, 1], 'min_max_range_parameter_1': [1, 1], 'paramDiscretization': [1, 1]})
+			print('Single realization mode! No sweeps intended here.')
 		else:
-			print('Implement this!')
+			print('Implement this! Check what was wanted.')
 			sys.exit()
 		scanValues, allPoints = setup.all_initial_phase_combinations(dict_pll, dict_net, dict_algo, paramDiscretization=dict_algo['paramDiscretization'])
 		print('allPoints:', [allPoints], '\nscanValues', scanValues)
@@ -264,7 +270,7 @@ def evaluate_pool_data(dict_net, dict_pll, dict_algo, pool_data):
 				plt.show()
 		plot.plot_order_param_vs_parameter_space(pool_data, average_time_order_parameter_in_periods=average_time_order_parameter_in_periods, colormap=cm.hsv)
 		plot.plot_final_phase_configuration_vs_parameter_space(pool_data, average_time_phase_difference_in_periods=average_time_order_parameter_in_periods,
-															phase_wrap=1, std_treshold_determine_time_dependency=0.15*np.pi,
+															phase_wrap=3, std_treshold_determine_time_dependency=0.15*np.pi,
 															std_treshold_order_param_determine_time_dependency=0.075, colormap=cm.hsv)
 		plt.draw()
 		plt.show()
@@ -379,6 +385,8 @@ def multihelper(iterConfig, initPhiPrime0, dict_net, dict_pll, dict_algo, param_
 			dict_pll_rea.update({param_id: change_param})							# update the parameter chosen in change_param with a value of all scanvalues
 		else:
 			print('No parameters for sweep specified -- hence simulating the same parameter set for all realizations!')
+		if 'entrain' in dict_net['topology'] and dict_pll['typeOfHist'] == 'syncState':  # set syncF to the reference frequency
+			dict_pll_rea.update({'syncF': dict_pll_rea['intrF'][0]})
 
 		return simulateSystem(dict_net_rea, dict_pll_rea, dict_algo_rea, multi_sim=True)
 
@@ -407,6 +415,8 @@ def multihelper(iterConfig, initPhiPrime0, dict_net, dict_pll, dict_algo, param_
 		else:
 			print('No parameters for sweep specified -- hence simulating the same parameter set for all realizations!')
 
+		if 'entrain' in dict_net['topology'] and dict_pll['typeOfHist'] == 'syncState':  # set syncF to the reference frequency
+			dict_pll_rea.update({'syncF': dict_pll_rea['intrF'][0]})
 		if not dict_net['phiInitConfig'] and 'entrain' in dict_net_rea['topology']:  # needs to be done here to catch this case below if there are np.nans!
 			print('\nPhase configuration of synchronized state will be set according to supplied topology and twist state information!')
 			setup.generate_phi0(dict_net_rea, dict_pll_rea, dict_algo_rea)

sim_pll/pll_lib.py
@@ -296,7 +296,7 @@ class SignalControlledOscillator:
 		self.d_phi = None
 		self.pll_id = pll_id
 		self.fric_coeff = get_from_value_or_list(pll_id, dict_pll['friction_coefficient'], dict_net['Nx'] * dict_net['Ny'])
-		self.sync_freq_rad 	= 2.0 * np.pi * get_from_value_or_list(pll_id, dict_pll['syncF'], dict_net['Nx'] * dict_net['Ny'])     #dict_pll['syncF']
+		self.sync_freq_rad = 2.0 * np.pi * get_from_value_or_list(pll_id, dict_pll['syncF'], dict_net['Nx'] * dict_net['Ny'])
 		if dict_pll['fric_coeff_PRE_vs_PRR'] == 'PRR':
 			self.intr_freq_rad = 2.0 * np.pi * get_from_value_or_list(pll_id, dict_pll['intrF'], dict_net['Nx'] * dict_net['Ny'])
 		elif dict_pll['fric_coeff_PRE_vs_PRR'] == 'PRE':
@@ -314,7 +314,8 @@ class SignalControlledOscillator:
 		self.response_vco = dict_pll['responseVCO']
 
 		if dict_pll['typeOfHist'] == 'syncState':
-			print('I am the VCO of PLL%i with intrinsic frequency f=%0.2f Hz and K=%0.2f Hz, initially in a synchronized state.' % (self.pll_id, self.intr_freq_rad / (2.0 * np.pi), self.K_rad / (2.0 * np.pi)))
+			print('I am the VCO of PLL%i with intrinsic frequency f=%0.2f Hz and K=%0.2f Hz, initially in a synchronized state with sync. frequency fsync=%0.2f Hz.' % (self.pll_id,
+																								self.intr_freq_rad / (2.0 * np.pi), self.K_rad / (2.0 * np.pi), self.sync_freq_rad / (2.0 * np.pi)))
 			self.init_freq = self.sync_freq_rad
 		elif dict_pll['typeOfHist'] == 'freeRunning':
 			print('I am the VCO of PLL%i with intrinsic frequency f=%0.2f Hz and K=%0.2f Hz, initially in free running.' % (self.pll_id, self.intr_freq_rad / (2.0 * np.pi), self.K_rad / (2.0 * np.pi)))

sim_pll/plot_lib.py
@@ -863,7 +863,7 @@ def plot_instantaneous_freqs_vs_time_dependent_parameter(dict_pll: dict, dict_ne
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-def plot_order_parameter_vs_time_dependent_parameter_div_and_undiv(dict_pll: dict, dict_net: dict, dict_data: dict):
+def plot_order_parameter_vs_time_dependent_parameter_div_and_undiv(dict_pll: dict, dict_net: dict, dict_data: dict, dict_algo: dict):
 	dict_pll, dict_net = prepareDictsForPlotting(dict_pll, dict_net)
 
 	opacity_reverse_change = 0.35
@@ -2204,7 +2204,7 @@ def plot_final_phase_configuration_vs_parameter_space(pool_data: dict, average_t
 	if phase_wrap == 1:  # plot phase-differences in [-pi, pi) interval
 		shift2piWin = np.pi
 		imshow_min_val = -np.pi
-		imshow_max_val = np.pi-0.000001
+		imshow_max_val = np.pi - 0.000001
 	elif phase_wrap == 2:  # plot phase-differences in [-pi/2, 3*pi/2) interval
 		shift2piWin = 0.5 * np.pi
 		imshow_min_val = -np.pi/2
@@ -2244,8 +2244,8 @@ def plot_final_phase_configuration_vs_parameter_space(pool_data: dict, average_t
 			averaging_time_as_index = np.int(average_time_phase_difference_in_periods * np.mean(pool_data[0][i]['dict_pll']['intrF']) / pool_data[0][i]['dict_pll']['dt'])
 		results.append(
 			[pool_data[0][i]['dict_data']['order_parameter'][-1], np.mean(pool_data[0][i]['dict_data']['order_parameter'][-averaging_time_as_index:]),		# 0 & 1
-			np.std(pool_data[0][i]['dict_data']['order_parameter'][-int(5.3*averaging_time_as_index):]),														# 2
-			np.std(pool_data[0][i]['dict_data']['order_parameter'][-int(8.3 * averaging_time_as_index):]),													# 3
+			np.std(pool_data[0][i]['dict_data']['order_parameter'][-int(10.3 * averaging_time_as_index):]),													# 2
+			np.std(pool_data[0][i]['dict_data']['order_parameter'][-int(20.6 * averaging_time_as_index):]),													# 3
 			np.min(pool_data[0][i]['dict_data']['order_parameter'][-int(5.3 * averaging_time_as_index):]),													# 4
 			np.max(pool_data[0][i]['dict_data']['order_parameter'][-int(5.3 * averaging_time_as_index):]),													# 5
 			np.min(pool_data[0][i]['dict_data']['order_parameter'][-int(10.6 * averaging_time_as_index):-int(5.3 * averaging_time_as_index)]),				# 6
@@ -2298,9 +2298,9 @@ def plot_final_phase_configuration_vs_parameter_space(pool_data: dict, average_t
 					else:
 						constantR_beta_kl[j, i] = np.nan
 					# then check, whether it is decreasing -- check whether std decreases AND whether min and max values increase/decrease respectively
-					if results[i, 3] <= results[i, 2] or (results[i, 4] > results[i, 6] or results[i, 5] < results[i, 7]):
+					if results[i, 3] <= results[i, 2]: 			# or (results[i, 4] > results[i, 6] or results[i, 5] < results[i, 7]):
 						decreasingR_beta_kl[j, i] = ((pool_data[0][i]['dict_data']['phi'][-1, j] - pool_data[0][i]['dict_data']['phi'][-1, 0] + shift2piWin) % (2.0 * np.pi)) - shift2piWin
-					elif results[i, 3] > results[i, 2] or (results[i, 4] < results[i, 6] or results[i, 5] > results[i, 7]):
+					elif results[i, 3] > results[i, 2]: 		# or (results[i, 4] < results[i, 6] or results[i, 5] > results[i, 7]):
 						decreasingR_beta_kl[j, i] = np.nan
 					else:
 						print('Recheck logic here in plot_final_phase_configuration_vs_parameter_space-fct!')
@@ -2349,10 +2349,19 @@ def plot_final_phase_configuration_vs_parameter_space(pool_data: dict, average_t
 						constantR_beta_kl[j, i] = ((pool_data[0][i]['dict_data']['phi'][-1, j] - pool_data[0][i]['dict_data']['phi'][-1, 0] + shift2piWin) % (2.0 * np.pi)) - shift2piWin
 					else:
 						constantR_beta_kl[j, i] = np.nan
-					# then check, whether it is decreasing -- check whether std decreases AND whether min and max values increase/decrease respectively
-					if results[i, 3] <= results[i, 2] or (results[i, 4] > results[i, 6] or results[i, 5] < results[i, 7]):
+					# then check, whether order parameter peak2peak is decreasing -- check whether std decreases AND whether min and max values increase/decrease respectively
+					if results[i, 3] <= results[i, 2]:			# and (results[i, 4] > results[i, 6] or results[i, 5] < results[i, 7]):		# standard deviation decreases, min and max values get closer to each other
+
+						# here change maybe... the peak to peak between max and min of the order paramerter should become smaller...
+						# solution 1) 	write a function that analyzes the time-series in the following way: smooth the data to average out noise and fluctuations and then
+						#				check whether the distance between minima and maxima in become smaller for larger times
+						# solution 2)	calculate the standard deviation over an interval that defines a running window and then analyze the time evolution of the
+						#				std as time advances -- this has the advantage that local extreme events/fluctuations do not yield incorrect classifiction,
+						#				we want to understand the overall trend of the std as time progresses
+
+
 						decreasingR_beta_kl[j, i] = ((pool_data[0][i]['dict_data']['phi'][-1, j] - pool_data[0][i]['dict_data']['phi'][-1, 0] + shift2piWin) % (2.0 * np.pi)) - shift2piWin
-					elif results[i, 3] > results[i, 2] or (results[i, 4] < results[i, 6] or results[i, 5] > results[i, 7]):
+					elif results[i, 3] > results[i, 2]:							# and (results[i, 4] < results[i, 6] or results[i, 5] > results[i, 7]):
 						decreasingR_beta_kl[j, i] = np.nan
 					else:
 						print('Recheck logic here in plot_final_phase_configuration_vs_parameter_space-fct!')
@@ -2467,7 +2476,7 @@ def plot_final_phase_configuration_vs_parameter_space(pool_data: dict, average_t
 
 		plt.imshow(tempresults.astype(float), interpolation='nearest', cmap=colormap, aspect='auto', origin='lower',
 				   extent=(dict_algo['min_max_range_parameter_0'][0], dict_algo['min_max_range_parameter_0'][1], dict_algo['min_max_range_parameter_1'][0], dict_algo['min_max_range_parameter_1'][1]),
-				   vmin=std_treshold_determine_time_dependency, vmax=2 * np.pi)
+				   vmin=imshow_min_val, vmax=imshow_max_val) #vmin=std_treshold_determine_time_dependency, vmax=2 * np.pi)
 		plt.title(r'$\Delta_\textrm{theory}$' + phi_string)
 		plt.xlabel(x_label)
 		plt.ylabel(y_label)
@@ -2493,7 +2502,7 @@ def plot_final_phase_configuration_vs_parameter_space(pool_data: dict, average_t
 
 		plt.imshow(tempresults_ma.astype(float), interpolation='nearest', cmap=colormap, aspect='auto', origin='lower',
 				   extent=(dict_algo['min_max_range_parameter_0'][0], dict_algo['min_max_range_parameter_0'][1], dict_algo['min_max_range_parameter_1'][0], dict_algo['min_max_range_parameter_1'][1]),
-				   vmin=std_treshold_determine_time_dependency, vmax=2 * np.pi)
+				   vmin=imshow_min_val, vmax=imshow_max_val) #vmin=std_treshold_determine_time_dependency, vmax=2 * np.pi)
 		plt.title(r'std($\Delta$' + phi_string + ')')
 		plt.xlabel(x_label)
 		plt.ylabel(y_label)
@@ -2517,7 +2526,7 @@ def plot_final_phase_configuration_vs_parameter_space(pool_data: dict, average_t
 
 		plt.imshow(tempresults_ma.astype(float), interpolation='nearest', cmap=colormap, aspect='auto', origin='lower',
 				   extent=(dict_algo['min_max_range_parameter_0'][0], dict_algo['min_max_range_parameter_0'][1], dict_algo['min_max_range_parameter_1'][0], dict_algo['min_max_range_parameter_1'][1]),
-				   vmin=std_treshold_determine_time_dependency, vmax=2 * np.pi)
+				   vmin=imshow_min_val, vmax=imshow_max_val) #vmin=std_treshold_determine_time_dependency, vmax=2 * np.pi)
 		plt.title(r'$\Delta$' + phi_string + r' for $R(t)\rightarrow\infty=c$')
 		plt.xlabel(x_label)
 		plt.ylabel(y_label)
@@ -2543,7 +2552,7 @@ def plot_final_phase_configuration_vs_parameter_space(pool_data: dict, average_t
 
 		plt.imshow(tempresults_ma.astype(float), interpolation='nearest', cmap=colormap, aspect='auto', origin='lower',
 				   extent=(dict_algo['min_max_range_parameter_0'][0], dict_algo['min_max_range_parameter_0'][1], dict_algo['min_max_range_parameter_1'][0], dict_algo['min_max_range_parameter_1'][1]),
-				   vmin=std_treshold_determine_time_dependency, vmax=2 * np.pi)
+				   vmin=imshow_min_val, vmax=imshow_max_val) #vmin=std_treshold_determine_time_dependency, vmax=2 * np.pi)
 		plt.title(r'$\Delta$' + phi_string + r' for $std(R(t))$ and min/max($R(t)$) increasing/decreasing')
 		plt.xlabel(x_label)
 		plt.ylabel(y_label)